bee470b59809315474e452bc5cbdc91fc83be61e
Crambeary <3926951+Crambeary@users.noreply.github.com>
2024-11-19 15:51:02 -0600

    imported basic fCC Personal Site

---
+++ styles.cc
@@ -0,0 +1,72 @@
+/* navbar stays on top */
+/* welcome section is the full viewport size */
+/* Use media queries to resize */
+
+*, ::before, ::after {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  background-color: #2a2a40;
+  color: #d4d4d4;
+}
+
+header {
+  display: flex;
+  height: 50px;
+  background-color: rgb(125, 125, 125);
+  align-items: center;
+  position: sticky;
+  top: 0;
+}
+
+header nav a {
+  color: white;
+  padding: 10px;
+  font-size: 1.1rem;
+  text-decoration: none;
+  font-family: Helvetica, sans-serif;
+}
+
+#welcome-section {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  font-family: Helvetica, sans-serif;
+}
+
+h2 {
+  font-family: Helvetica, sans-serif;
+  display: flex;
+  justify-content: center;
+}
+
+#projects {
+  display: flex;
+  justify-content: space-evenly;
+  font-family: Helvetica, sans-serif;
+}
+
+.project-tile {
+  margin: 10px;
+  padding: 10px;
+  width: 50%;
+  border: 1px solid;
+}
+
+footer {
+  height: 50px;
+  background-color: rgb(125, 125, 125);
+  color: white;
+  align-items: center;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+@media (max-contet: 500px) {
+  #projects {
+    flex-direction: column;
+  }
+}
